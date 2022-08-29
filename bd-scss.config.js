6564845c84de23b75006528bcb9c22ded103bd24
Ian <Gibbu_@hotmail.com>
2022-08-29 18:45:25 +1000

    Updated to new repo structure

---
+++ bd-scss.config.js
@@ -0,0 +1,16 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: 'MinimalCord',
+		author: 'Gibbu',
+		version: '2.2.0',
+		description: 'Changes Discord enough to give it a fresh feel while also making it darker. Supports both Light and Dark themes.',
+		source: 'https://github.com/DiscordStyles/MinimalCord',
+		invite: 'ZHthyCw',
+		authorId: '174868361040232448'
+	},
+	baseImport: 'https://discordstyles.github.io/MinimalCord/dist/MinimalCord.css',
+	base: {
+		output: 'dist/dist'
+	},
+}
