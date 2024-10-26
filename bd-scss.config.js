56e8746ad2673a020d28c9e8af25bb04b498bb4e
Gibbu <Gibbu_@hotmail.com>
2024-10-26 12:24:22 +1100

    Fixed chatbox + formatting

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -4,13 +4,14 @@ export default {
 		name: 'MinimalCord',
 		author: 'Gibbu',
 		version: '2.3.0',
-		description: 'Changes Discord enough to give it a fresh feel while also making it darker. Supports both Light and Dark themes.',
+		description:
+			'Changes Discord enough to give it a fresh feel while also making it darker. Supports both Light and Dark themes.',
 		source: 'https://github.com/DiscordStyles/MinimalCord',
 		invite: 'ZHthyCw',
-		authorId: '174868361040232448'
+		authorId: '174868361040232448',
 	},
 	baseImport: 'https://discordstyles.github.io/MinimalCord/dist/MinimalCord.css',
 	base: {
-		output: 'dist/dist'
+		output: 'dist/dist',
 	},
-}
+};
